6ff3e053c64fa8477eaac61fe7fe98e5cadd2f33
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-02-14 22:27:49 +0530

    Navbar and Landing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
-
+import Header from "./Components/Header/header";
+import Footer from "./Components/Footer/footer";
+import LandingPage from "./Screens/LandingPage/LandingPage";
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <LandingPage />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
