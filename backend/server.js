06b7a7ac2df9fc8de7c6aa3435ef0edc27dbca66
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-02-16 00:07:20 +0530

    User Model,Backend Login and Register API

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoute");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
 const app = express();
 dotenv.config();
 connectDB();
-
-const port = process.env.PORT || 5000;
+app.use(express.json());
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
@@ -19,6 +21,11 @@ app.get("/api/notes/:id", (req, res) => {
   const note = notes.find((n) => n._id === req.params.id);
   res.json(note);
 });
+
+app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
