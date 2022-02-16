d6a7ce4df5b847c5f0f300587e8b12c75b00a661
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-02-16 18:44:27 +0530

    Completed LoginUI

---
+++ frontend/src/Screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import Mainscreen from "../../Components/Mainscreen";
+import Loading from "../../Components/Loading";
+import ErrorMessage from "../../Components/ErrorMessage";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./LoginScreen.css";
+
+function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      setLoading(true);
+
+      const { data } = await axios.post(
+        "/api/users/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
+      console.log(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <Mainscreen title="LOGIN">
+        <div className="loginContainer">
+          {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+          {loading && <Loading />}
+          <Form onSubmit={submitHandler}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+            ></Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          <Row className="py-3">
+            <Col>
+              New Customer ? <Link to="/register">Register Here</Link>
+            </Col>
+          </Row>
+        </div>
+      </Mainscreen>
+    </div>
+  );
+}
+
+export default LoginScreen;
