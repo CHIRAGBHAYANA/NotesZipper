de90156b3378e566427d1cee8b6daad94864e993
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-02-16 22:54:33 +0530

    Completed Register UI with cloudinary

--- frontend/src/Screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/Screens/RegisterScreen/RegisterScreen.js
@@ -1,7 +1,158 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Mainscreen from "../../Components/Mainscreen";
+import Loading from "../../Components/Loading";
+import ErrorMessage from "../../Components/ErrorMessage";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
 function RegisterScreen() {
-  return <div>RegisterScreen</div>;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [pic, setPic] = useState(
+    "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+  );
+
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+  const [picMessage, setPicMessage] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(email);
+    if (password !== confirmPassword) {
+      setMessage("Password do not Match");
+    } else {
+      setMessage(null);
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+        setLoading(true);
+        const { data } = await axios.post(
+          "/api/users",
+          { name, pic, email, password },
+          config
+        );
+        setLoading(false);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+      } catch (error) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  const postDetails = (pics) => {
+    if (!pics) {
+      return setPicMessage("Please select an Image");
+    }
+    setPicMessage(null);
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "notezipper");
+      data.append("cloud_name", "chiragbhayana");
+      fetch("https://api.cloudinary.com/v1_1/chiragbhayana/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setPic(data.url.toString());
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      return setPicMessage("Please Select an Image");
+    }
+  };
+
+  return (
+    <div>
+      <Mainscreen title={"REGISTER"}>
+        <div className="loginContainer">
+          {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+          {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+          {loading && <Loading />}
+
+          <Form onSubmit={submitHandler}>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                value={name}
+                placeholder="Enter name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                value={email}
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="confirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                value={confirmPassword}
+                placeholder="Password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            {picMessage && (
+              <ErrorMessage variant="danger">{picMessage}</ErrorMessage>
+            )}
+
+            <Form.Group className="mb-3" controlId="pic">
+              <Form.Label>Profile Picture</Form.Label>
+              {/* <Form.File
+              id="custom-file"
+              type="image/png"
+              label="Upload Profile Picture"
+              custom
+              /> */}
+              <Form.Control
+                type="file"
+                onChange={(e) => postDetails(e.target.files[0])}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Register
+            </Button>
+          </Form>
+          <Row className="py-3">
+            <Col>
+              Have an Account ? <Link to="/register">Login</Link>
+            </Col>
+          </Row>
+        </div>
+      </Mainscreen>
+    </div>
+  );
 }
 
 export default RegisterScreen;
