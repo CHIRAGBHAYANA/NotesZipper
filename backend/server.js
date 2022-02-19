7f26d3c49efa51dfc97c359b75b7cb8a7512c5cf
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-02-19 13:05:38 +0530

    Added CRUD Operation for Task

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const notes = require("./data/notes");
+// const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoute");
+const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
@@ -10,19 +11,17 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-
 app.get("/", (req, res) => {
   res.send("hello");
 });
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.json(note);
-});
+
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+//   res.json(note);
+// });
 
 app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 app.use(notFound);
 app.use(errorHandler);
 const port = process.env.PORT || 5000;
